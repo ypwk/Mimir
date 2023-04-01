7d9fa05c0d1b251a67847781e14b89c4fa0fa88e
Kevin Wu <28658818+ypwk@users.noreply.github.com>
2023-04-01 17:42:37 -0400

    imcomplete premise adding
    Co-Authored-By: quislu <105848323+quislu@users.noreply.github.com>

--- amplify/backend/function/mimirSolver/src/index.py
+++ amplify/backend/function/mimirSolver/src/index.py
@@ -1,15 +1,78 @@
 import json
+from collections import deque
 
 def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+    """
+    'Server-side' code that handles events. Quis code here. Will call solve at some point
+
+    Arguments:
+    event   — An object that contains data for a Lambda function to process. A python dictionary in the following format:
+    {
+      "premises": list of premises in string format.
+      "target": a single string target
+    }
+
+    Example:
+        ["(A ∨ (A ∨ B)) → A", "A → ( B ∨ C)"]
+
+        expression = (un_connective expression) | (expression bin_connective expression) | sentence
+        sentence = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z" | “?”
+        bin_connective = "∨" | "∧" | "→" | "↔"
+        un_connective = "¬"
+
+    context — Provides methods and properties that provide information about the invocation, function, and runtime
+              environment.
+
+    Return: {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        },
+        "body": json.dumps([[rule, e1, e2, result], [rule, e1, e2, result], ... , [rule, e1, e2, result]]),
+    }
+
+    See this link for more information: https://docs.aws.amazon.com/lambda/latest/dg/python-handler.html
+    """
+
+    print("received event:")
+    print(event)
+
+    # unpack premises
+
+    # call solve
+
+    # https://docs.python.org/3/library/queue.html#queue.Queue
+
+
+    # process output of solve, put into output format
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        },
+        "body": json.dumps("Hello from your new Amplify Python lambda!"),
+    }
+
+
+class Expression:
+
+    def __init__(self, sentence_1, connective = None, sentence_2 = None):
+        self.sentence_1 = sentence_1
+        self.connective = connective
+        self.sentence_2 = sentence_2
+
+
+# class Expression:
+
+def solve(premises, target):
+
+
+    while True:
+      if target in premises:
+
+    # make decisions based on parsed input (recur)
--- src/pages/Solver.js
+++ src/pages/Solver.js
@@ -1,10 +1,54 @@
+import React, { useState } from "react";
 
-import React from 'react'
+import Editor from "../components/Editor";
 
-import Editor from '../components/Editor'
+import { NodeSelection, Transaction } from "@tiptap/pm/state";
 
 export default function Solver() {
+  const [premises, setPremises] = useState([]);
+
+  let premise_render = [];
+  let key = 0;
+  premises.forEach((p) => {
+    premise_render.push(<p key={key}>{p}</p>);
+    key += 1;
+  });
+
   return (
-    <Editor />
-  )
+    <>
+      <h3>Input Target:</h3>
+      <Editor
+        enterCallback={() => {
+          console.log("fhajisd");
+        }}
+      />
+
+      <h3>Input Premises:</h3>
+      <Editor
+        enterCallback={(editor_view) => {
+          const content = editor_view.state.doc.textContent;
+          if (content !== "") {
+            console.log(editor_view);
+            let allSelect = new NodeSelection(editor_view.state.doc.resolve(0));
+            console.log(allSelect)
+            let trans = editor_view.state.tr
+            trans.setSelection(allSelect)
+            trans.deleteSelection()
+            editor_view.state.apply(trans)
+            setPremises((premises) => {
+              let new_premises = premises.map((x) => x);
+              new_premises.push(content);
+              return new_premises;
+            });
+          }
+        }}
+      />
+
+      <button className="menubutton">Proof</button>
+
+      {premise_render}
+
+      <h3>Output:</h3>
+    </>
+  );
 }
